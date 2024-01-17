0f48e93fc8b7e4bd689e447ed50a8d75b7a930e1
Yuxuan Jiang <jiangy10@rose-hulman.edu>
2024-01-17 17:07:06 -0500

    adapter
    Co-authored-by: rhit-wallisjm <wallisjm@rose-hulman.edu>

--- hospital-at-home-provider-side-web/src/Components/Chat/ChatFrame.tsx
+++ hospital-at-home-provider-side-web/src/Components/Chat/ChatFrame.tsx
@@ -1,6 +1,20 @@
-import React from 'react'
-export default function ChatFrame():React.JSX.Element{
-	return(
-		<div>this is a chat frame</div>
-	);
+import React, {useEffect, useState} from 'react'
+import {
+	ChatAdapter, ChatComposite,
+	useAzureCommunicationCallAdapter,
+	useAzureCommunicationChatAdapter
+} from "@azure/communication-react";
+
+export default function ChatFrame({adapter}: {adapter: ChatAdapter | undefined}):React.JSX.Element{
+	
+	if (adapter) {
+		return(
+			<div>
+				<ChatComposite adapter={adapter}></ChatComposite>
+			</div>
+		);
+	} else {
+		return(<div></div>)
+	}
+	
 }
