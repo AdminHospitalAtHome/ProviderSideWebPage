ed0145ba71b683d89352e511d8b9e88202990303
rhit-wallisjm <wallisjm@rose-hulman.edu>
2024-01-18 13:40:56 -0500

    Chat Working!!!!!!!!!
    Co-Authored-By: rhit-jiangy10 <90019374+jiangy10@users.noreply.github.com>

--- hospital-at-home-provider-side-web/src/Components/Chat/ChatFrame.tsx
+++ hospital-at-home-provider-side-web/src/Components/Chat/ChatFrame.tsx
@@ -1,59 +1,46 @@
-import React, {useEffect, useMemo, useState} from 'react'
+import React, {useMemo} from 'react'
 import {
-	ChatComposite, fromFlatCommunicationIdentifier,
-
-	useAzureCommunicationChatAdapter
+  ChatAdapter,
+  ChatComposite, fromFlatCommunicationIdentifier,
+  useAzureCommunicationChatAdapter
 } from "@azure/communication-react";
 import {ChatThreadClient} from "@azure/communication-chat";
-import {endpointurl, getCommunicationToken} from '../../BackendFunctionCall/Message'
+import {endpointUrl} from '../../BackendFunctionCall/Message'
 import {AzureCommunicationTokenCredential, CommunicationUserIdentifier} from "@azure/communication-common";
 
-export default function ChatFrame({thread, communicationID}: {thread: ChatThreadClient, communicationID: string}):React.JSX.Element {
+export default function ChatFrame({thread, communicationID, communicationToken}: {
+  thread: ChatThreadClient,
+  communicationID: string,
+  communicationToken: string
+}): React.JSX.Element {
 
+  const userId = useMemo(() => {
+    return fromFlatCommunicationIdentifier(communicationID) as CommunicationUserIdentifier
+  }, [communicationID]);
 
-	const [credential, setCredential] = useState<any>(undefined);
-	// const userID = fromFlatCommunicationIdentifier(communicationID) as CommunicationUserIdentifier
+  const credential = useMemo(() => {
+    return new AzureCommunicationTokenCredential(communicationToken);
+  }, [communicationToken]);
 
-	const userId = useMemo( () => {
-		return fromFlatCommunicationIdentifier(communicationID) as CommunicationUserIdentifier
-	}, [communicationID])
+  const adapter: ChatAdapter | undefined = useAzureCommunicationChatAdapter({
+    endpoint: endpointUrl,
+    userId: userId,
+    displayName: "TEMP NAME",
+    credential: credential,
+    threadId: thread.threadId
+  });
 
-	// const credential = useMemo(async () => {
-	// 	await getCommunicationToken(communicationID).then((cred) => {
-	// 		return (new AzureCommunicationTokenCredential(cred));
-	// 	})
-	// 	console.log('Thread Created')
-	// }, [communicationID])
-
-	useEffect(() => {
-		getCommunicationToken(communicationID).then((cred) => {
-			setCredential(new AzureCommunicationTokenCredential(cred));
-		})
-	}, [thread])
-
-
-	console.log("Run")
-
-	const adapter = useAzureCommunicationChatAdapter({
-		endpoint: endpointurl,
-		userId: userId,
-		displayName: "TEMP NAME",
-		credential: credential,
-		threadId: thread.threadId
-	})
-	// let adapter = ""
-
-
-	if (adapter) {
-		return (
-			<div>
-				<ChatComposite adapter={adapter}></ChatComposite>
-			</div>
-		);
-	} else {
-		return (<div></div>);
-	}
-
-
-
+  if (adapter) {
+    return (
+      <div>
+        <ChatComposite adapter={adapter}
+                       options={{
+                         errorBar: true
+                       }}
+        ></ChatComposite>
+      </div>
+    );
+  } else {
+    return <div/>
+  }
 }
--- hospital-at-home-provider-side-web/src/Pages/ChatPage.tsx
+++ hospital-at-home-provider-side-web/src/Pages/ChatPage.tsx
@@ -1,63 +1,57 @@
 import ChatMenu from "../Components/Chat/ChatMenu";
 import ChatFrame from "../Components/Chat/ChatFrame";
 import React, {useEffect, useState} from "react";
-import {ChatAdapter} from "@azure/communication-react";
 import {ChatClient, ChatThreadClient} from "@azure/communication-chat";
-import {getAllThreads, initChatClient, temp_communicationId} from "../BackendFunctionCall/Message";
-
+import {
+  getAllThreads,
+  getCommunicationToken,
+  initChatClient,
+  temp_communicationId
+} from "../BackendFunctionCall/Message";
 
 
 export default function ChatPage(): React.JSX.Element {
-	// TODO: Add paramater for ThreadID
-	//doc link: https://azure.github.io/communication-ui-library/?path=/docs/composites-chat-basicexample--basic-example
-	const providerId: number = 300000001
-	const patientId: number = 200000001
-	const [chatClient, setChatClient] = useState<ChatClient | undefined>(undefined);
-	const [threadClients, setThreadClients] = useState<ChatThreadClient[]>([]);
-	const [chatThread, setChatThread] = useState<ChatThreadClient | undefined>(undefined);
+  //doc link: https://azure.github.io/communication-ui-library/?path=/docs/composites-chat-basicexample--basic-example
+  const providerId: number = 300000001
+  const [chatClient, setChatClient] = useState<ChatClient | undefined>(undefined);
+  const [threadClients, setThreadClients] = useState<ChatThreadClient[]>([]);
+  const [chatThread, setChatThread] = useState<ChatThreadClient | undefined>(undefined);
+  const [communicationToken, setCommunicationToken] = useState<string>("")
 
-	useEffect(() => {
-		console.log("Run");
-		initChatClient(providerId).then(res => {
-			setChatClient(res);
-		});
-	}, []);
+  useEffect(() => {
+    initChatClient(providerId).then(res => {
+      setChatClient(res);
+      getCommunicationToken(temp_communicationId).then(setCommunicationToken)
+    });
 
-	useEffect(() => {
-		console.log("GetAllThreads");
-		if (chatClient) {
-			getAllThreads(chatClient).then((res) => {
-				setThreadClients(res);
-			})
-		}
-	},[chatClient])
+  }, []);
 
-	useEffect(() => {
-		if (threadClients.length !== 0) {
-			setChatThread(threadClients[0])
-		}
-	}, [threadClients]);
+  useEffect(() => {
+    if (chatClient) {
+      getAllThreads(chatClient).then((res) => {
+        setThreadClients(res);
+      })
+    }
+  }, [chatClient])
 
 
-
-
-
-
-
-	if (chatThread) {
-		return (<body style={{paddingTop: '60px', display: 'flex', flexDirection: 'row'}}>
-		<div style={{flexGrow: 1}}><ChatMenu threadClients={threadClients} setThread={setChatThread}></ChatMenu></div>
-		<div style={{flexGrow: 11}}>
-			<ChatFrame thread={chatThread} communicationID={temp_communicationId}></ChatFrame>
-		</div>
-		</body>);
-	} else {
-		return (
-		<body style={{paddingTop: '60px', display: 'flex', flexDirection: 'row'}}>
-		<div style={{flexGrow: 11}}><ChatMenu threadClients={threadClients} setThread={setChatThread}></ChatMenu></div>
-
-		</body>);
-	}
+  if (chatThread && communicationToken !== "") {
+    return (<body style={{paddingTop: '60px', display: 'flex', flexDirection: 'row'}}>
+    <div style={{flexGrow: 1}}><ChatMenu threadClients={threadClients} setThread={setChatThread}></ChatMenu></div>
+    <div style={{flexGrow: 11}}>
+      <ChatFrame thread={chatThread} communicationID={temp_communicationId}
+                 communicationToken={communicationToken}></ChatFrame>
+    </div>
+    </body>);
+  } else {
+    return (
+      <body style={{paddingTop: '60px', display: 'flex', flexDirection: 'row'}}>
+      <div style={{flexGrow: 1}}><ChatMenu threadClients={threadClients} setThread={setChatThread}></ChatMenu></div>
+      <div style={{flexGrow: 11}}>
+        Please Select a Chat...
+      </div>
+      </body>);
+  }
 
 
 }
