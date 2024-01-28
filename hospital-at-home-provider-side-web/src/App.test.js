4a62bdda39a41ca3ac92961b1ebeec05eea48b38
rhit-wallisjm <wallisjm@rose-hulman.edu>
2024-01-28 16:22:00 -0500

    FIRST TEST WORKS???

--- hospital-at-home-provider-side-web/src/App.test.js
+++ hospital-at-home-provider-side-web/src/App.test.js
@@ -1,8 +1,20 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import {getCommunicationId, getCommunicationToken} from "./BackendFunctionCall/Message";
+import {expect, jest, test} from '@jest/globals'
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+
+// Mocking Learned from Stack Overflow
+// https://stackoverflow.com/questions/64818305/simple-fetch-mock-using-typescript-and-jest
+
+test('Init Chat Client', () => {
+    jest.spyOn(global, 'fetch').mockImplementationOnce(
+        jest.fn(
+            () => Promise.resolve({
+                json: () => Promise.resolve(
+                    [{FirstName: "Jake", LastName: "Bob", CommunicationId: "ABCD"}])
+            }),
+        )
+    )
+
+    expect(getCommunicationId(1000)).resolves.toBe("ABCD");
+
+})
